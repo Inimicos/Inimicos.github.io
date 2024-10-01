38393f1b3c05b29c73a5dccbe92b4ba83eb0d7f4
Inimicos <jn.birkemeyer@protonmail.com>
2024-10-01 14:55:43 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  leinwand = createCanvas(windowWidth, windowHeight);
+  leinwand.position(0,0)
+  leinwand.style("z-index","-1");
+
+  fill(0,0,100,100);
+  background(220);
+  frameRate(2);
+  noStroke();
+}
+
+function draw() {
+  
+  circle(mouseX,mouseY,50);
+  
+}
